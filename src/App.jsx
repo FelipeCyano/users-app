110c68449233843ed83aa1c4dd12ead65183898a
FelipeCyano <felipecyano@gmail.com>
2022-12-25 23:09:08 -0300

    feat: themes and fonts config.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import AppRoutes from './routes';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './styles/global.css';
 
 export default function App() {
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#422C76'
+      },
+      secondary: {
+        main: '#FF2f69'
+      }
+    }
+  });
+
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </ThemeProvider>
   )
 }
