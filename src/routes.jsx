1057243f53296aec9ca4293cfb4ebab51cc42b80
FelipeCyano <felipecyano@gmail.com>
2022-12-25 23:06:27 -0300

    feat: routes config.

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import UsersList from './pages/UsersList';
+
+function AppRoutes() {
+  return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/users" element={<UsersList />} />
+      </Routes>
+  )
+}
+
+export default AppRoutes;
